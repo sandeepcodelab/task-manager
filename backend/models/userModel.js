e7202101d3ba386faa21eeb57a7a255392c57631
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-17 18:00:15 +0530

    add token generation and password check functionalities

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -16,10 +18,51 @@ const userSchema = new Schema(
             type: String,
             required: true,
 
-        }
+        },
+        refreshToken: {
+            type: String,
+        },
     },
     {timestamps: true}
 )
+
+
+// Password bcrypt
+userSchema.pre("save", async function(next){
+    
+    if(!this.isModified("password")) return next();
+
+        await bcrypt.hash(this.password, 10);
+
+    next()
+})
+
+//Check password
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+// Generate access and refresh token
+userSchema.methods.generateAccessToken = function() {
+
+    return jwt.sign({
+                id: this._id,
+                name: this.name,
+                email: this.email
+            }, 
+            process.env.ACCESS_TOKEN,
+            { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
+}
+
+userSchema.methods.generateRefreshToken = function() {
+
+    return jwt.sign({
+                id: this._id
+            }, 
+            process.env.REFRESH_TOKEN,
+            { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
+}
+
 
 const User = mongoose.model('User', userSchema);
 
