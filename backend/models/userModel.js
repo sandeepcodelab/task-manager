fa3629c00dd4d0e720444bf84da247a533fb5a4d
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-18 13:26:54 +0530

    fix password hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,7 +32,7 @@ userSchema.pre("save", async function(next){
     
     if(!this.isModified("password")) return next();
 
-        await bcrypt.hash(this.password, 10);
+        this.password = await bcrypt.hash(this.password, 10);
 
     next()
 })
