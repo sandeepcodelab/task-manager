2c4ff0359fa54bb349d407ae44f70ce33ef637c0
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-19 14:44:41 +0530

    add task controller

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,168 @@
+import Task from "../models/taskModel.js";
+
+const addTask = async(req, res) => {
+
+    try {
+        
+        if(!req.body){
+            return res.status(400).json({
+                success: false,
+                message: "Request body cannot be empty."
+            })
+        }
+
+        const{title, description} = req.body
+
+        if(!title){
+            return res.status(400).json({
+                success: false,
+                message: "Title is required"
+            })
+        }
+
+        const task = await Task.create({
+            title,
+            description,
+            owner: req.user?._id
+        })
+        
+        return res.status(201).json({
+                success: true,
+                message: "New task created successfully",
+                task
+            })
+
+    } catch (error) {
+        console.log("add task: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Somthing went wrong while creating a new task.",
+        })
+    }
+}
+
+const getTasks = async(req, res) => {
+    try {
+        
+        const task = await Task.find({owner: req.user?._id})
+
+        if(!task){
+            return res.status(404).json({
+                success: false,
+                message: "Task not found.",
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Tasks fetched successfully.",
+            tasks: task
+        })
+
+    } catch (error) {
+        console.log("all tasks: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Somthing went wrong while fetching tasks.",
+        })
+    }
+}
+
+const updateTask = async(req, res) => {
+
+    try {
+    
+        if(!req.body){
+            return res.status(400).json({
+                success: false,
+                message: "Request body cannot be empty."
+            })
+        }
+
+        const{title, description, completed} = req.body
+        
+        if(!title){
+            return res.status(400).json({
+                success: false,
+                message: "Title is required"
+            })
+        }
+        
+        const taskId = req.params?.id;
+
+        const task = await Task.findById(taskId)
+        
+        if(!task) return res.status(404).json({success: false, message: "Task not found."})
+            
+        if(task.owner.toString() !== req.user._id.toString()){
+            return res.status(401).json({
+                message: "Unauthorized"
+            })
+        }
+
+        const updatedTask = await Task.findByIdAndUpdate(
+            taskId,
+            {
+                $set: {
+                    title: title || task.title,
+                    description: description || task.description,
+                    completed: completed !== undefined ? completed : task.completed
+                }
+            },
+            {new: true}
+        )
+        
+        return res.status(200).json({
+                success: true,
+                message: "Task updated successfully",
+                updatedTask
+            })
+
+    } catch (error) {
+        console.log("update task: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Somthing went wrong while updating a task.",
+        })
+    }
+}
+
+const deleteTask = async(req, res) => {
+
+    try {
+
+        const taskId = req.params?.id;
+
+        const task = await Task.findById(taskId)
+
+        if(!task) return res.status(404).json({success: false, message: "Task not found."})
+        
+        if(task.owner.toString() !== req.user._id.toString()){
+            return res.status(401).json({
+                message: "Unauthorized"
+            })
+        }
+
+        const delete_task = await Task.findByIdAndDelete(taskId)
+        
+        return res.status(200).json({
+                success: true,
+                message: "Task deleted successfully",
+            })
+
+    } catch (error) {
+        console.log("dalate task: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Somthing went wrong while deleting a task.",
+        })
+    }
+}
+
+
+export {
+    addTask,
+    getTasks,
+    updateTask,
+    deleteTask
+}
