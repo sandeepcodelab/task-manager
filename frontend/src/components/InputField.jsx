5101ca8b320b02a0a5da488bd56435187f52a92d
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-22 16:25:17 +0530

    update input field

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,15 +1,15 @@
 function InputField({type="text", placeholder, className, label, ...props}){
 
     return (
-        <div>
-            <label className="block text-gray-300 mb-1 font-medium">{label}</label>
-            <input
-                type={type}
-                placeholder={placeholder}
-                className={`w-full px-4 py-3 border border-gray-600 bg-gray-900/70 text-gray-200 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition ${className}`}
-                {...props}
-            />
-        </div>
+        <>
+        <label className="block text-gray-300 font-medium">{label}</label>
+        <input
+            type={type}
+            placeholder={placeholder}
+            className={`w-full px-4 py-3 border border-gray-600 bg-gray-900/70 text-gray-200 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition ${className}`}
+            {...props}
+        />
+        </>
     )
 }
 
