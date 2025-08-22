be141bb653b33100841bdaaaf85d9582ec69712b
sandeepcodelab <sandeep.codes1@gmail.com>
2025-08-22 18:28:24 +0530

    add validation and api request

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,9 +2,75 @@ import Container from "../components/Container/Container";
 import InputField from "../components/InputField";
 import Button from "../components/Button";
 import { Link } from "react-router";
+import { useState } from "react";
+import axios from "axios";
 
 function Register() {
-  
+
+  const[name, setName] = useState("")
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+  const[confirmPassword, setConfirmPassword] = useState("")
+  const[errors, setErrors] = useState({})
+  const[apiRes, setApiRes] = useState({})
+  const[apiErr, setApiErr] = useState({})
+  const[loader, setLoader] = useState(false)
+
+
+  const validate = () => {
+
+    let formErrors = {};
+    const regEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i;
+
+    if(!name){
+      formErrors.name = "Name is required";
+    }
+    if(!email){
+      formErrors.email = "Email is required";
+    }
+    if(email){
+      if(!regEmail.test(email)){
+        formErrors.email = "Invalid email id";
+      }
+    }
+    if(!password){
+      formErrors.password = "Password is required";
+    }
+    if(!confirmPassword){
+      formErrors.conPass = "Confirm Password is required";
+    }
+    if(password !== confirmPassword){
+      formErrors.conPass = "Confirm Password does not match";
+    }
+
+    return formErrors;
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setApiErr({})
+    setApiRes({})
+    
+    const formValidation = validate();
+    setErrors(formValidation)
+    
+    if(Object.keys(formValidation).length > 0) return;
+
+    axios.post("/api/v1/user/register",{
+      name,
+      email,
+      password
+    })
+    .then((response) => {
+      setApiRes(response)
+    })
+    .catch((err) => {
+      setApiErr(err.response)
+    })
+  }
+
   return (
 
     <Container>
@@ -17,11 +83,63 @@ function Register() {
             Join us and start your journey today
           </p>
 
-          <form className="space-y-5">
+          {
+            // console.log(apiErr.data)
+            (apiErr?.data?.message) ? <p className="text-red-500 text-center font-bold pb-2">{apiErr?.data?.message}</p> : null
+          }
+          {/* {
+            console.log(apiRes.data)
+            (apiRes?.data?.message) ? <p className="text-green-500 text-center font-bold pb-2">{apiRes?.data?.message}</p> : null
+          } */}
 
-            <InputField type="text" placeholder="John Deo" label="Name"/>
-            <InputField type="email" placeholder="you@example.com" label="Email"/>
-            <InputField type="password" placeholder="••••••••" label="Password"/>
+          <form className="space-y-5" onSubmit={handleSubmit}>
+
+            <div>
+              <InputField 
+                type="text" 
+                placeholder="Enter your name" 
+                label="Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            
+              {
+                errors.name ? <span className="text-red-500 text-sm">{errors.name}</span> : null
+              }
+            </div>
+            <div>
+              <InputField 
+                type="email" 
+                placeholder="Enter your email" 
+                label="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                
+                {
+                  errors.email ? <span className="text-red-500 text-sm">{errors.email}</span> : null
+                }
+            </div>
+            <div>
+              <InputField 
+                type="password" 
+                placeholder="Enter password" 
+                label="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {
+                errors.password ? <span className="text-red-500 text-sm">{errors.password}</span> : null
+              }
+            </div>
+            <div>
+              <InputField 
+                type="password" 
+                placeholder="Enter confirm password" 
+                label="Confirm Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              {
+                errors.conPass ? <span className="text-red-500 text-sm">{errors.conPass}</span> : null
+              }
+            </div>
 
             <Button
               type="submit"
